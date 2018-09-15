0ed24fda48ba6fddb9a836eed89bd07fae409977
jenn.ward <wardjl1985@gmail.com>
2018-09-15 18:52:53 -0400

    additional files added to js tutorial

--- javascript_tutorials/basic_array/basic_array.js
+++ javascript_tutorials/basic_array/basic_array.js
@@ -1,25 +1,27 @@
 var playList = [
-    'I Did It My Way',
-    'Respect',
-    'Imagine',
-    'Born to Run',
-    'Louie Louie',
-    'Maybellene'
+    ['I Did It My Way', 'Frank Sinatra'],
+    ['Respect', 'Aretha Franklin'],
+    ['Imagine', 'John Lennon'],
+    ['Born to Run', 'Bruce Springsteen'],
+    ['Louie Louie', 'The Kingsmen'], 
+    ['Maybellene', 'Chuck Berry']
   ];
   
   function print(message) {
     document.write(message);
   }
-
-  function printList( list ) {
-      var listHTML = '<ol>';
-      for ( var i = 0; i < list.length; i += 1){
-          listHTML += '<li>' + list[i] + '</li>';
-      }
-      listHTML += '</ol>';
-      print(listHTML);
+  
+  function printSongs( songs ) {
+    var listHTML = '<ol>';
+    for ( var i = 0; i < songs.length; i += 1) {
+      listHTML += '<li>' + songs[i][0] + ' by ' + songs[i][1] + '</li>';
     }
-    printList(playList);
+    listHTML += '</ol>';
+    document.write(listHTML);
+  }
+  
+  printSongs(playList);
+
 /*push adds to the end of an array
 unshift adds to the beginning
 pop removes from the end
--- javascript_tutorials/search_webpage_array/search_webpage_array.js
+++ javascript_tutorials/search_webpage_array/search_webpage_array.js
@@ -7,7 +7,7 @@ function print(message) {
 
 while (true) {
     search = prompt("Search for a product in our store. Type 'list' to show all of the produce and 'quit' to exit.");
-    search = search.toLowerCase(;)
+    search = search.toLowerCase();
     if (search === 'quit') {
     break;
     } else if (search === 'list'){
