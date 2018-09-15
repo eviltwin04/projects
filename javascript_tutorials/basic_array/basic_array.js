313a90a0de5a4e5a09b99b712e09585da2fcffbf
jenn.ward <wardjl1985@gmail.com>
2018-09-15 15:45:06 -0400

    updates to js tutorials

---
+++ javascript_tutorials/basic_array/basic_array.js
@@ -0,0 +1,10 @@
+var shoppinglist = ['carrots', 'milk', 'eggs']
+console.log(shoppinglist[0]);
+console.log(shoppinglist[1]);
+console.log(shoppinglist[2]);
+/*push adds to the end of an array
+unshift adds to the beginning
+pop removes from the end
+shift removes from the beginning*/ 
+
+shoppinglist.push('gummies');
