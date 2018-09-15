9034b50b97e055c6bb21b0b8fc9703f62c85d8c2
jenn.ward <wardjl1985@gmail.com>
2018-09-15 16:56:33 -0400

    updating js tutorials with html integration

--- javascript_tutorials/basic_array/basic_array.js
+++ javascript_tutorials/basic_array/basic_array.js
@@ -1,10 +1,26 @@
-var shoppinglist = ['carrots', 'milk', 'eggs']
-console.log(shoppinglist[0]);
-console.log(shoppinglist[1]);
-console.log(shoppinglist[2]);
+var playList = [
+    'I Did It My Way',
+    'Respect',
+    'Imagine',
+    'Born to Run',
+    'Louie Louie',
+    'Maybellene'
+  ];
+  
+  function print(message) {
+    document.write(message);
+  }
+
+  function printList( list ) {
+      var listHTML = '<ol>';
+      for ( var i = 0; i < list.length; i += 1){
+          listHTML += '<li>' + list[i] + '</li>';
+      }
+      listHTML += '</ol>';
+      print(listHTML);
+    }
+    printList(playList);
 /*push adds to the end of an array
 unshift adds to the beginning
 pop removes from the end
 shift removes from the beginning*/ 
-
-shoppinglist.push('gummies');
