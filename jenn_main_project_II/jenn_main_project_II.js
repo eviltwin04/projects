ebf81c0ffffe32361dec87033fd07f8efd07bcb2
jenn.ward <wardjl1985@gmail.com>
2018-11-15 13:25:59 -0500

    additional style changes added

--- jenn_main_project_II/jenn_main_project_II.js
+++ jenn_main_project_II/jenn_main_project_II.js
@@ -1,3 +1,5 @@
+// This javascript is for my main headers to change from upper to lowercase 
+
 var header2 = document.getElementsByTagName('h2')
 // var btn= document.getElementsByClassName('btn')
 
@@ -11,20 +13,50 @@ header2[i].addEventListener('mouseout' , () => {
 });
 }
 
-var hasBeenClicked = false;
-jQuery('#button1').click(function () {
-    hasBeenClicked = true;
+ 
+// This javascript is for my form submission announcement 
+
+function button1() {
+    $('#formSubmitted').show().slideDown(700).delay(2000).slideUp(700);
+}
+
+
+// This javascript is for my Resume PDF checkbox
+
+const $odd = $('a:odd');
+const $secureLinks = $('a[href^="https://"]');
+//^ means 'starts with. $ in the below context means the string at the end of an attribute value
+
+const $pdfs = $('a[href$="pdf"]');
+
+
+$secureLinks.attr('target', '_blank');
+$pdfs.attr('download', true);
+
+$secureLinks.addClass('secure');
+
+$pdfs.addClass('pdf');
+
+$pdfs.on('click', function(){
+   
+//check to see if chheckbox has been checked
+//if zero checkboxes have been checked
+if ($(':checked').length === 0) {
+//prevent download of document
+event.preventDefault();
+//alert the user
+alert('Please check the box to allow PDF downloads');
+}
+
 });
-      
-if (hasBeenClicked) {
-    $('#formSubmitted').hide().slideDown(700).delay(2000).slideUp(700);
-} else {
-    $('#formSubmitted').hide();
-}     
+
+// $('#links').apend($pdfCheckbox);
+
+$('a').each(function(index, link) {
+    // console.log(index, $(element).attr('href'));
+});
 
 
-//  $('#button1').click(function() {
-//  if  $('#formSubmitted').hide().slideDown(700).delay(2000).slideUp(700); 
-//     });
+
 
 
