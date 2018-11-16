d42cabf46fada9ae416e147ce6b1d76fa2e4dfca
jenn.ward <wardjl1985@gmail.com>
2018-11-15 19:02:23 -0500

    final touches added css and jquery

--- jenn_main_project_II/jenn_main_project_II.js
+++ jenn_main_project_II/jenn_main_project_II.js
@@ -1,12 +1,35 @@
-
 $(document).ready(function() {
-	$('.togglenav').click(function(e) {
-		$('togglenav').toggleClass('active');
+	$('.toggle-nav').click(function(e) {
+		$('.toggle-nav').toggleClass('active');
 		$('.mainNav ul').toggleClass('active');
 
 		e.preventDefault();
 	});
 });
+
+// This javascript is for my form submission announcement 
+
+function button1() {
+    $('#formSubmitted').show().slideDown(700).delay(2000).slideUp(700);
+}
+
+// This javascript is for my form
+
+$(function(){
+	$('#emailButton').on('click', emailMe);
+});
+
+function emailMe(){
+	var mailTo = 'mailto:wardjl1985@gmail.com?';
+  var subject = 'subject=Site Contact';
+  if($('#contactMe').is(':checked')){
+  	subject += ' - Please Reply';
+  }
+  subject = subject.replace(' ', '%20');
+  var message = '&body=' + $('#msg').val() + '%0D%0A%0D%0A' + $('#name').val();
+  window.location =  mailTo + subject + message;
+}
+
 
 // // This javascript is for my main headers to change from upper to lowercase 
 
@@ -22,14 +45,6 @@ header2[i].addEventListener('mouseout' , () => {
     header2[i].textContent = header2[i].textContent.toLowerCase();
 });
 }
-
- 
-// This javascript is for my form submission announcement 
-
-function button1() {
-    $('#formSubmitted').show().slideDown(700).delay(2000).slideUp(700);
-}
-
 
 
 // This javascript is for my Resume PDF checkbox
