3f38565c44242f3ae83e2a17949bad7ecc068033
jenn.ward <wardjl1985@gmail.com>
2019-02-03 13:52:02 -0500

    Added Point bool to map.cs

--- TowerGame/Point.cs
+++ TowerGame/Point.cs
@@ -1,8 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+namespace TowerGame
 {
-	public Class1()
-	{
-	}
+    class Point
+    {
+        public readonly int X;
+        public readonly int Y;
+
+        public Point(int x, int y)
+
+        {
+            X = x;
+            y = y;
+       }
+    }
 }
