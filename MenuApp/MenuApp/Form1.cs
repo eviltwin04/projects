ee09766f6abfbea5396a2a26a02abb1d7588088f
Jennifer Lauren Ward <42983299+eviltwin04@users.noreply.github.com>
2019-03-27 15:14:32 -0400

    Added MenuApp
    Randomizer application

---
+++ MenuApp/MenuApp/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenuApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            MenuMaker menu = new MenuMaker() { Randomizer = new Random()};
+
+            label1.Text = menu.GetMenuItem();
+            label2.Text = menu.GetMenuItem();
+            label3.Text = menu.GetMenuItem();
+            label4.Text = menu.GetMenuItem();
+            label5.Text = menu.GetMenuItem();
+            label6.Text = menu.GetMenuItem();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MenuApp/MenuApp/MenuMaker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MenuApp
+{
+
+    class MenuMaker 
+    {
+        public Random Randomizer;
+
+        string[] Meats = { "Roast beef", "Salami", "Turkey", "Ham", "Pastrami", "Tofurky" };
+        string[] Condiments = { "yellow mustard", "brown mustard", "honey mustard", "mayo", "relish", "french dressing" };
+        string[] Breads = { "Rye", "White", "French", "Pumpernickle", "wheat", "a roll" };
+
+        public string GetMenuItem()
+        {
+
+            string randomMeat = Meats[Randomizer.Next(Meats.Length)];
+            string randomCondiments = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBreads = Breads[Randomizer.Next(Breads.Length)];
+            return randomMeat + " with " + randomCondiments + " on " + randomBreads;
+        }
+
+    }
+}
+
