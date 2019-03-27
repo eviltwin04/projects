6377e3a883b253fe51ed633e30b599ced4e2e57a
Jennifer Lauren Ward <42983299+eviltwin04@users.noreply.github.com>
2019-03-27 13:46:35 -0400

    Added Elephant project correctly

---
+++ ElephantProject/ElephantProject/Elephant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ElephantProject
+{
+    public class Elephant
+    {
+        public int EarSize;
+        public string Name;
+
+        public void WhoAmI()
+        {
+            MessageBox.Show("My name is " + Name + " and my ears are " + EarSize + " inches wide!", Name + " says:");
+        }
+    }
+}
