0aea26223cf5e6895bf313d80ca57989d7d2b55a
Jennifer Lauren Ward <42983299+eviltwin04@users.noreply.github.com>
2019-04-05 12:44:45 -0400

    Added farmerApp

---
+++ FarmerApp/FarmerApp/Farmer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmerApp
+{
+    class Farmer
+    {
+        public int MyProperty { get; set; }
+        private int feedMultiplier;
+        public int FeedMultiplier { get { return feedMultiplier; } }
+        public int BagsOfFeed { get; set; }
+        public Farmer (int numberOfCows, int feedMultiplier)
+        {
+            this.feedMultiplier = feedMultiplier;
+            NumberOfCows = numberOfCows;
+        }
+
+        private int numberOfCows;
+        public int NumberOfCows
+        {
+            get
+            {
+                return numberOfCows;
+            }
+            set 
+            {
+                numberOfCows = value;
+                BagsOfFeed = numberOfCows * FeedMultiplier;
+            }
+        }
+    }
+
+}
---
+++ FarmerApp/FarmerApp/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace FarmerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.cowLabel = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(45, 6);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // cowLabel
+            // 
+            this.cowLabel.AutoSize = true;
+            this.cowLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cowLabel.Location = new System.Drawing.Point(8, 8);
+            this.cowLabel.Name = "cowLabel";
+            this.cowLabel.Size = new System.Drawing.Size(37, 13);
+            this.cowLabel.TabIndex = 1;
+            this.cowLabel.Text = "Cows";
+            this.cowLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(45, 41);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(75, 23);
+            this.calculateButton.TabIndex = 2;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(178, 114);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.cowLabel);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label cowLabel;
+        private System.Windows.Forms.Button calculateButton;
+    }
+}
+
